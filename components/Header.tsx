a774ac1d84e364d746422aba5d83975bc86d90a4
Maksim Hodasevich <maksim.hodasevich@gmail.com>
2022-09-26 22:55:55 +0200

    init

---
+++ components/Header.tsx
@@ -0,0 +1,11 @@
+// import { Box } from "native-base";
+
+export const Header = () => {
+  return (
+    <div>
+      {/* <Box> */}
+        hedwepofjwepofwepofwpoefwefwef
+      {/* </Box> */}
+    </div>
+  )
+}
