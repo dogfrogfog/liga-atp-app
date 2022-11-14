a0e915039591ca9d480dc972acb6790a10c73177
Maksim Hodasevich <maksim.hodasevich@gmail.com>
2022-11-14 17:26:25 +0100

    review fixes

--- components/admin/DataForm.tsx
+++ components/admin/DataForm.tsx
@@ -10,74 +10,74 @@ import {AddPlayerFormType, AddPlayerSchema} from "../../zod/admin/addPlayer";
 import {zodResolver} from "@hookform/resolvers/zod";
 
 interface IDataFormProps {
-    type: 'players' | 'matches' | 'tournaments';
-    formTitle: string;
-    onSubmit: any;
-    onClose: any;
-    editingRow?: PlayerT | TournamentT;
+  type: 'players' | 'matches' | 'tournaments';
+  formTitle: string;
+  onSubmit: any;
+  onClose: any;
+  editingRow?: PlayerT | TournamentT;
 }
 
 const getField = (props: any, register: any, errors:any) => {
-    switch (props.type) {
-        case 'file':
-        case 'checkbox': {
-            return (
-                <>
-                    <span>{props.placeholder}</span>
-                    <input
-                        type={props.type}
-                        {...register(props.name, { required: props.required })}
-                    />
-                    {errors[props.name] && (<p>Error</p>)}
-                </>
-            );
-        };
-        case 'select': {
-            return (
-                <select name={props.name} {...register(props.name, { required: props.required })}>
-                    {Object.entries(props.options).map(([key, value]) => (
-                        <option value={key}>{value as ReactNode}</option>
-                    ))}
-                </select>
-            )
-        }
-        default: {
-            return (
-                <input
-                    placeholder={props.placeholder}
-                    type={props.type}
-                    {...register(props.name, { required: props.required })}
-                />
-            )
-        };
+  switch (props.type) {
+    case 'file':
+    case 'checkbox': {
+      return (
+        <>
+          <span>{props.placeholder}</span>
+          <input
+            type={props.type}
+            {...register(props.name, { required: props.required })}
+          />
+          {errors[props.name] && (<p>Error</p>)}
+        </>
+      );
+    };
+    case 'select': {
+      return (
+        <select name={props.name} {...register(props.name, { required: props.required })}>
+          {Object.entries(props.options).map(([key, value]) => (
+            <option value={key}>{value as ReactNode}</option>
+          ))}
+        </select>
+      )
+    }
+    default: {
+      return (
+        <input
+          placeholder={props.placeholder}
+          type={props.type}
+          {...register(props.name, { required: props.required })}
+        />
+      )
+    };
     }
 }
 
 // todo: add validation + errors
 const DataForm = ({
-                      onSubmit,
-                      onClose,
-                      editingRow,
-                      type,
-                      formTitle,
-                  }: IDataFormProps) => {
-    const { register, handleSubmit, watch , formState:{errors}} = useForm<AddPlayerFormType>({
-        resolver:zodResolver(AddPlayerSchema)
-    });
+  onSubmit,
+  onClose,
+  editingRow,
+  type,
+  formTitle,
+}: IDataFormProps) => {
+  const { register, handleSubmit, watch, formState: { errors } } = useForm<AddPlayerFormType>({
+    resolver: zodResolver(AddPlayerSchema)
+  });
 
-    return (
-        <Modal title={formTitle} handleClose={onClose}>
-            <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
-                {FORM_VALUES[type].map((props) => (
-                    <div key={props.name} className={styles.input}>
-                        {getField(props, register, errors)}
-                    </div>
-                ))}
-                <input className={styles.submit} type="submit" />
-            </form>
-            <pre>{JSON.stringify(watch(), null, 2)}</pre>
-        </Modal>
-    )
+  return (
+    <Modal title={formTitle} handleClose={onClose}>
+      <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+        {FORM_VALUES[type].map((props) => (
+          <div key={props.name} className={styles.input}>
+            {getField(props, register, errors)}
+          </div>
+        ))}
+        <input className={styles.submit} type="submit" />
+      </form>
+      <pre>{JSON.stringify(watch(), null, 2)}</pre>
+    </Modal>
+  );
 }
 
 export default DataForm
