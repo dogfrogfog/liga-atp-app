de4e15267a7a119332b6ecf6780d42a5ff3ae6a6
Maksim Hodasevich <maksim.hodasevich@gmail.com>
2022-11-15 17:49:18 +0100

    add logic description

--- pages/admin/tournaments/[pid].tsx
+++ pages/admin/tournaments/[pid].tsx
@@ -11,7 +11,7 @@ import {
 import PageTitle from 'ui-kit/PageTitle';
 import { updateTournament } from 'services/tournaments';
 import styles from './AdminSingleTournamentPape.module.scss';
-import { AiFillCodeSandboxCircle } from 'react-icons/ai';
+
 
 interface IAdminSingleTournamentPapeProps {
   tournament: TournamentT;
@@ -33,9 +33,12 @@ const AdminSingleTournamentPape: NextPage<IAdminSingleTournamentPapeProps> = ({
   };
 
   const updateDrawType = async () => {
+    const { tournament_players, match, ...rest } = activeTournament;
+
     const newTournament = await updateTournament({
-      ...activeTournament,
+      ...rest,
       draw_type: drawType,
+      draw: '',
     });
 
     if (newTournament.isOk) {
@@ -194,9 +197,40 @@ interface ITournamentDrawProps {
   matches: MatchT[];
 }
 
+const getStagesFromBracket = () => {
+
+};
+// !!!старые данные будут уже упорядочены!!!
+// 
+// как их можно парсить:
+// 1. у нас есть tournament.draw_type соответственно есть размер сетки
+// 2. зная размер сетки мы можем определить количество матчей и построить сетку
+//
+// зная количества матчей в одном уровне рисовать сетку по принципу: 
+// пример турнир q32
+// 1 стадия. первых 16 элементов массива match
+// 2 стаадия. 16 + 8
+// 3 сталия. 16 + 8 + 4 и тд
+// нужно проверить данные для даблс
+
+// для новых турниров можно сразу формировать draw!!
+// 1. у нас есть draw_type соответственно есть размер размер сетки
+// 2. зная размер сетки мы можем определить количество матчей и построить сетку
+// 3. заполняем сетку пустыми значениями (или алгоритмом который будет создавать объекты самостоятельно)
+// 4. первый раунд будет списан после окончательного формирования, следовательно можно считать 
+
+
 const TournamentDraw = ({ brackets, matches }: ITournamentDrawProps) => {
-  // console.log('brackets, matches:')
-  // console.log(brackets, matches);
+  // tournament with 819 id has all the props
+  const a = brackets.reduce((acc, { id }) => {
+    acc.push(parseInt(id, 2))
+    return acc;
+  }, [] as number[]);
+
+  // should
+
+  // console.log(brackets)
+  // console.log(a)
 
   return (
     <div className={styles.drawContainer}>
@@ -252,6 +286,10 @@ export const getServerSideProps = async (ctx: any) => {
 
   return {
     props: {
+      // todo: move match and tournament_players props out of tournament
+      // should be:
+      // matches: match,
+      // tournamentPlayers: tournament_players,
       tournament,
       players,
     },
