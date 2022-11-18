f90d274bd1022b78e1e09e1487bfe1f29f614bff
Илья Семёнов <71377999+Yny4ii@users.noreply.github.com>
2022-11-19 01:12:10 +0300

    feat: add welcoming component, add /preview page which shows tis preview (#22)
    * feat: preview component
    
    * refactor previe
    
    * refactor preview page
    
    Co-authored-by: Maksim Hodasevich <maksim.hodasevich@gmail.com>

---
+++ pages/preview.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from 'next';
+
+import PreviewComponent from 'components/PreviewComponent';
+
+const PreviewPage: NextPage = () => {
+  return (
+    <PreviewComponent
+      title="Добро пожаловать"
+    />
+  );
+};
+
+export default PreviewPage;
