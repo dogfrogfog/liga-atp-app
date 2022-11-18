71b047c28cc0c0b64fe09d20dd44c8319f3fb7a1
Maksim Hodasevich <maksim.hodasevich@gmail.com>
2022-11-18 09:48:50 +0100

    fix

--- components/admin/Pagination.tsx
+++ components/admin/Pagination.tsx
@@ -16,6 +16,7 @@ const Pagination = ({ pagination, setPagination }: IPaginationProps) => (
   <div className={styles.pagination}>
     <div className={styles.arrows}>
       <button
+        disabled={pagination.pageIndex === 0}
         onClick={() => setPagination((v) => ({ ...v, pageIndex: 0}))}
       >
         {'<<'}
@@ -28,11 +29,14 @@ const Pagination = ({ pagination, setPagination }: IPaginationProps) => (
       </button>
       <span className={styles.currentPage}>{pagination.pageIndex + 1}</span>
       <button
+        disabled={false}
         onClick={() => setPagination((v) => ({ ...v, pageIndex: pagination.pageIndex + 1 }))}
       >
         {'>'}
       </button>
-      <button onClick={() => setPagination((v) => ({ ...v, pageIndex: 850/Math.ceil(pagination.pageSize)}))}
+      <button
+        disabled={false}
+        onClick={() => setPagination((v) => ({ ...v, pageIndex: 850 / Math.ceil(pagination.pageSize) }))}
       >
         {'>>'}
       </button>
