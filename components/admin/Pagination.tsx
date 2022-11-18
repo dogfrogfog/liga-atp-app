724248a9bafa8e9f3c0b9456dfbbfc0823adbf35
Илья Семёнов <71377999+Yny4ii@users.noreply.github.com>
2022-11-18 12:02:50 +0300

    feat: add additional buttons and styles to pagination (#19)
    * refactor: error component
    
    * refactor: redesign form
    
    * refactor: code style
    
    * refactor: code style
    
    * refactor: code style
    
    * fix
    
    Co-authored-by: Maksim Hodasevich <maksim.hodasevich@gmail.com>

--- components/admin/Pagination.tsx
+++ components/admin/Pagination.tsx
@@ -16,15 +16,29 @@ const Pagination = ({ pagination, setPagination }: IPaginationProps) => (
   <div className={styles.pagination}>
     <div className={styles.arrows}>
       <button
+        disabled={pagination.pageIndex === 0}
+        onClick={() => setPagination((v) => ({ ...v, pageIndex: 0}))}
+      >
+        {'<<'}
+      </button>
+      <button
         onClick={() => setPagination((v) => ({ ...v, pageIndex: pagination.pageIndex - 1 }))}
         disabled={pagination.pageIndex === 0}
       >
         {'<'}
       </button>
+      <span className={styles.currentPage}>{pagination.pageIndex + 1}</span>
       <button
+        disabled={false}
         onClick={() => setPagination((v) => ({ ...v, pageIndex: pagination.pageIndex + 1 }))}
       >
         {'>'}
+      </button>
+      <button
+        disabled={false}
+        onClick={() => setPagination((v) => ({ ...v, pageIndex: 850 / Math.ceil(pagination.pageSize) }))}
+      >
+        {'>>'}
       </button>
       <select
         value={pagination.pageSize}
@@ -39,7 +53,6 @@ const Pagination = ({ pagination, setPagination }: IPaginationProps) => (
         ))}
       </select>
     </div>
-    <span className={styles.currentPage}>Страница: {pagination.pageIndex + 1}</span>
   </div>
 )
 
