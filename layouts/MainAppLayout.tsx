54172a53e83bfde1766140f2bf417cbcc536765f
Илья Семёнов <71377999+Yny4ii@users.noreply.github.com>
2022-11-21 21:40:15 +0300

    feat: redesign bottom menu (#27)
    * feat: add icons
    
    * refactor: change icons

--- layouts/MainAppLayout.tsx
+++ layouts/MainAppLayout.tsx
@@ -2,13 +2,10 @@ import type { ReactNode } from 'react'
 import Link from 'next/link'
 import cl from 'classnames'
 import { useRouter } from 'next/router'
-import { FiUsers } from 'react-icons/fi'
-import { AiOutlineUnorderedList } from 'react-icons/ai'
-import { BiNews } from 'react-icons/bi'
-import { TbMilitaryRank } from 'react-icons/tb'
 import { HiViewList } from 'react-icons/hi'
 
 import styles from './MainAppLayout.module.scss'
+import { Digest, HeadToHead, Players, Rating, Tournaments } from "./MainAppLayoutIcons";
 
 function MainAppLayout({ children }: { children: ReactNode }) {
   const currentRoute = useRouter().pathname
@@ -23,36 +20,28 @@ function MainAppLayout({ children }: { children: ReactNode }) {
       </div>
       <div className={styles.bottomMenu}>
         <Link href="/players">
-          <div className={cl(styles.menuItem, currentRoute === '/players' ? styles.active : '')}>
-            <FiUsers />
-            Игроки
+          <div className={cl(styles.menuItem, currentRoute === '/players' ? styles.active : styles.passive)}>
+            <Players />
           </div>
         </Link>
-        <Link
-          href="/tournaments"
-          className={currentRoute === '/tournaments' ? styles.active : ''}
-        >
-          <div className={cl(styles.menuItem, currentRoute === '/tournaments' ? styles.active : '')}>
-            <AiOutlineUnorderedList />
-            Турниры
+        <Link href="/tournaments">
+          <div className={cl(styles.menuItem, currentRoute === '/tournaments' ? styles.active : styles.passive)}>
+            <Tournaments/>
           </div>
         </Link>
         <Link href="/digest">
           <div className={cl(styles.menuItem, currentRoute === '/digest' ? styles.active : '')}>
-            <BiNews />
-            Дайдж
+            <Digest/>
           </div>
         </Link>
         <Link href="/elo">
           <div className={cl(styles.menuItem, currentRoute === '/elo' ? styles.active : '')}>
-            <TbMilitaryRank />
-            Эло
+            <Rating />
           </div>
         </Link>
         <Link href="/h2h">
           <div className={cl(styles.menuItem, currentRoute === '/h2h' ? styles.active : '')}>
-            <FiUsers />
-            H2H
+            <HeadToHead />
           </div>
         </Link>
         <Link href="/other">
