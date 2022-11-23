858072cad2b088569f7d0f4d84afb8f99abe4a6a
ilya <ilya.semyonov.03@mail.ru>
2022-11-23 17:39:58 +0300

    refactor: code style & react/icons

--- pages/players/index.tsx
+++ pages/players/index.tsx
@@ -10,34 +10,18 @@ import { PrismaClient } from '@prisma/client'
 import Input from 'ui-kit/Input';
 import { LEVEL_NUMBER_VALUES } from 'constants/values';
 import styles from 'styles/Players.module.scss';
+import { FiMenu, FiSearch } from 'react-icons/fi'
+import { BsFillPersonFill } from 'react-icons/bs'
 
 // should be nested from schema
 interface PlayersPageProps {
   players: any[];
 }
 
-const noAvatarIcon =
-  <svg width="42" height="42" viewBox="0 0 42 42" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <circle cx="21" cy="21" r="20.5" stroke="white"/>
-    <path d="M17.3844 17.5645H24.6479C28.3385 17.5645 31.4806 20.2494 32.0562 23.8948L32.5935 27.2982C33.0248 30.0295 30.9138 32.5 28.1486 32.5H13.8717C11.1003 32.5 8.98764 30.019 9.42924 27.283L9.98019 23.8695C10.5667 20.2357 13.7035 17.5645 17.3844 17.5645Z" fill="#C6C6C6" stroke="white"/>
-    <path d="M27 14.129C27 17.8052 24.117 20.7581 20.5938 20.7581C17.0705 20.7581 14.1875 17.8052 14.1875 14.129C14.1875 10.4528 17.0705 7.5 20.5938 7.5C24.117 7.5 27 10.4528 27 14.129Z" fill="#C6C6C6" stroke="white"/>
-  </svg>
-
-const searchIcon =
-  <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <path d="M23.414 20.586L18.337 15.509C19.386 13.928 20 12.035 20 10C20 4.486 15.514 0 10 0C4.486 0 0 4.486 0 10C0 15.514 4.486 20 10 20C12.035 20 13.928 19.386 15.509 18.337L20.586 23.414C21.366 24.195 22.634 24.195 23.414 23.414C24.195 22.633 24.195 21.367 23.414 20.586ZM3 10C3 6.14 6.14 3 10 3C13.86 3 17 6.14 17 10C17 13.86 13.86 17 10 17C6.14 17 3 13.86 3 10Z" fill="#BBBBBB"/>
-  </svg>
-
-const filterIcon =
-  <svg width="24" height="20" viewBox="0 0 24 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-    <rect width="24" height="4" rx="2" fill="#BBBBBB"/>
-    <rect y="8" width="24" height="4" rx="2" fill="#BBBBBB"/>
-    <rect y="16" width="24" height="4" rx="2" fill="#BBBBBB"/>
-  </svg>
-
 const Players: NextPage<PlayersPageProps> = ({ players }) => {
   const [search, setSearch] = useState('');
   const [data, setData] = useState(players)
+  const [isOpen, setIsOpen] = useState(false);
 
   if (data.length > 0) {
     console.log(data)
@@ -54,8 +38,6 @@ const Players: NextPage<PlayersPageProps> = ({ players }) => {
       setData(response.data)
     }
   }
-
-  const [isOpen, setIsOpen] = useState(false);
   return (
     <div className={styles.playersContainer}>
       <div className={styles.header}>
@@ -63,7 +45,7 @@ const Players: NextPage<PlayersPageProps> = ({ players }) => {
       </div>
       {!isOpen && <div className={styles.search}>
         <button onClick={submitSearch} className={styles.searchButton}>
-          {searchIcon}
+          <FiSearch/>
         </button>
         <Input
           placeholder="Введите имя игрока"
@@ -71,17 +53,17 @@ const Players: NextPage<PlayersPageProps> = ({ players }) => {
           onChange={handleSearch}
         />
         <button onClick={() => setIsOpen(true)} className={styles.filterButton}>
-          {filterIcon}
+          <FiMenu/>
         </button>
       </div>}
       {isOpen && <div className={styles.popupContainer}>
         <button onClick={() => setIsOpen(false)} className={styles.filterButton}>
-          {filterIcon}
+          <FiMenu/>
         </button>
         <ul>
           <p>Фильтр по категориям</p>
-          {['Топ 10 игроков Лиги', 'Рейтинг ЭЛО', 'Количество сыгранных матчей', 'Процент выигранных тай-брейков', 'Среднее число ударов'].map( e => (
-            <li key={e}>{e}</li>
+          {['Топ 10 игроков Лиги', 'Рейтинг ЭЛО', 'Количество сыгранных матчей', 'Процент выигранных тай-брейков', 'Среднее число ударов'].map(v => (
+            <li key={v}>{v}</li>
           ))}
         </ul>
       </div>}
@@ -102,7 +84,7 @@ const Players: NextPage<PlayersPageProps> = ({ players }) => {
                   <td>
                     <div className={styles.userInfo}>
                       <div className={styles.userImage}>
-                        {avatar ?  <Image src={ligaLogo} alt={first_name + ' ' + last_name}/> : noAvatarIcon}
+                        {avatar ?  <Image src={ligaLogo} alt={first_name + ' ' + last_name}/> : <BsFillPersonFill/>}
                       </div>
                         <p>{first_name}<br/>{last_name}</p>
                     </div>
