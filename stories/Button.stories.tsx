0b7c1526d45681c27e02845e009f3841ee1fc03e
Maksim Hodasevich <maksim.hodasevich@gmail.com>
2022-12-04 11:07:26 +0100

    add styles and props for button ui component

--- stories/Button.stories.tsx
+++ stories/Button.stories.tsx
@@ -5,9 +5,6 @@ import Button from '../ui-kit/Button';
 export default {
   title: 'Button/Button',
   component: Button,
-  argTypes: {
-    backgroundColor: { control: 'color' },
-  },
 } as ComponentMeta<typeof Button>;
 
 const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
@@ -15,9 +12,5 @@ const Template: ComponentStory<typeof Button> = (args) => <Button {...args} />;
 export const Primary = Template.bind({});
 Primary.args = {
   children: 'primary',
-};
-
-export const Secondary = Template.bind({});
-Secondary.args = {
-  children: 'secondary',
+  type: 'primary',
 };
