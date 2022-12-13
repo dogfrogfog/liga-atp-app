5650e5bddeb38ab9e3e14ee8f3584a7e14a64be6
Maksim Hodasevich <maksim.hodasevich@gmail.com>
2022-12-13 11:18:46 +0100

    fix player page crush if no level value passed

--- constants/values.ts
+++ constants/values.ts
@@ -1,56 +1,50 @@
-// interface NumberValuesT {
-//   [k: number | string]: number;
-// }
-// todo: add types objects
+type NumberValuesT = {
+  [k: number]: string;
+}
 
-export const LEVEL_NUMBER_VALUES: any = {
+export const LEVEL_NUMBER_VALUES: NumberValuesT = {
   4: 'leger',
   3: 'pro',
   2: 'masters',
   1: 'challenger',
   0: 'futures',
-  '-1': 'Satellite', // todo: change index, reorder indexes
+  [-1]: 'Satellite',
 };
 
-export const TOURNAMENT_TYPE_NUMBER_VALUES: any = {
+// previously used indexes: 8, 9, 10, 16
+export const TOURNAMENT_TYPE_NUMBER_VALUES: NumberValuesT = {
   0: 'Большой Шлем',
   1: 'Про',
   2: 'Мастерс',
-  11: 'Супермастерс', // new
+  11: 'Супермастерс',
   3: 'Челленджер',
   4: 'Фьючерс',
   5: 'Сателлит',
   6: 'Леджер',
   7: 'Тайбрейк',
-
-  // previous used ids: 8, 9, 10, 16
-
   12: 'Парный Сателлит',
-  13: 'Парный Суперсателлит', // new
+  13: 'Парный Суперсателлит', 
   14: 'Парный Фьючерс',
-  15: 'Парный Леджер', // new
+  15: 'Парный Леджер', 
   18: 'Парный Челенджер',
   20: 'Парный Мастерс',
   22: 'Парный Про',
-
-  100: 'Итоговый Турнир', // new
-  101: 'Парный ProAm', // new
+  100: 'Итоговый Турнир',
+  101: 'Парный ProAm',
 };
 
-export const TOURNAMENT_DRAW_TYPE_NUMBER_VALUES: any = {
-  // olympic net (singles types)
+// previously used indexes: 22, 5, 7, 6, 8, 9 
+export const TOURNAMENT_DRAW_TYPE_NUMBER_VALUES: NumberValuesT = {
+  // olympic net
   10: 'D8',
   3: 'D16',
   1: 'D32',
-  12: 'D64', // new
+  12: 'D64',
   11: 'D8 + Q',
   4: 'D16 + Q',
   2: 'D32 + Q',
-  13: 'D64 + Q', // new
-
-  //  22, 5, 7, 6, 8, 9 // used indexes (deprecated)
-
-  // groups net (doubles types)
+  13: 'D64 + Q',
+  // groups net
   14: 'G6',
   15: 'G8',
   16: 'G10',
@@ -58,13 +52,13 @@ export const TOURNAMENT_DRAW_TYPE_NUMBER_VALUES: any = {
   18: 'G16',
 };
 
-export const SURFACE_TYPE_NUMBER_VALUES: any = {
+export const SURFACE_TYPE_NUMBER_VALUES: NumberValuesT = {
   0: 'Хард',
   1: 'Грунт',
   2: 'Трава',
 };
 
-export const TOURNAMENT_STATUS_NUMBER_VALUES = {
+export const TOURNAMENT_STATUS_NUMBER_VALUES: NumberValuesT = {
   1: 'Запись',
   2: 'Идет',
   3: 'Завершен',
