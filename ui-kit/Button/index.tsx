2aa09a7a1669ca2dae300125a998ed2f74b6ae23
maksim hodasevich <47758224+dogfrogfog@users.noreply.github.com>
2022-12-30 14:36:18 +0100

    feat: add input with suggestions to ui-kit (#75)
    * add basic input with suggestions
    
    * add SuggestionsInput component
    
    * add styles to suggestions input

--- ui-kit/Button/index.tsx
+++ ui-kit/Button/index.tsx
@@ -2,11 +2,11 @@ import type { ReactNode } from 'react';
 
 import styles from './styles.module.scss';
 
-interface IButtonProps {
+type ButtonProps = {
   children: ReactNode;
-}
+};
 
-const Button = ({ children }: IButtonProps) => {
+const Button = ({ children }: ButtonProps) => {
   return <button className={styles.button}>{children}</button>;
 };
 
