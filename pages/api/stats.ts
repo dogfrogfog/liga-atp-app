ef6b2602ca96ba9190a73acad079e052a026ae5e
maksim hodasevich <47758224+dogfrogfog@users.noreply.github.com>
2023-01-02 10:37:51 +0100

    chore: refactor api folder structure (#77)

--- pages/api/stats.ts
+++ pages/api/stats.ts
@@ -1,22 +1,24 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { PrismaClient, player } from '@prisma/client';
-import { AiOutlineConsoleSql } from 'react-icons/ai';
+import { player as PlayerT } from '@prisma/client';
 
-export default async (
-  req: NextApiRequest,
-  res: NextApiResponse<player[] | player>
-) => {
-  // if (req.method === 'GET') {
-  //   const paginatedPlayers = await prisma.player.findMany({
-  //     take: parseInt(req.query.take as string),
-  //     skip: parseInt(req.query.skip as string),
-  //   })
-  //   res.json(paginatedPlayers)
-  // }
-  // if (req.method === 'POST') {
-  //   const createdPlayer = await prisma.player.create({
-  //     data: req.body.data,
-  //   })
-  //   res.json(createdPlayer)
-  // }
+import { prisma } from 'services/db';
+
+// should return stats for single player+ for two players if it's h2h page
+export default async (req: NextApiRequest, res: NextApiResponse<PlayerT[]>) => {
+  if (req.method === 'GET') {
+    const { player1Id, player2Id } = req.query;
+
+    // const h2hPlayers = await prisma.player.findMany({
+    //   where: {
+    //     id: {
+    //       in: [
+    //         parseInt(player1Id as string, 10),
+    //         parseInt(player2Id as string, 10),
+    //       ],
+    //     },
+    //   },
+    // });
+
+    res.json({} as any);
+  }
 };
