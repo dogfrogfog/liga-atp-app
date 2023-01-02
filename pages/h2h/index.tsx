e106311a8c30b217f19610d05dfd48874926dc28
maksim hodasevich <47758224+dogfrogfog@users.noreply.github.com>
2023-01-02 10:28:11 +0100

    feat: add H2H index page (#76)
    * add basic template for h2h
    
    * add styles to /h2h index page
    
    * disable link when has not 2 selected players (0 or 1)
    
    * add folder h2h
    
    * fix suggestions input position

---
+++ pages/h2h/index.tsx
@@ -0,0 +1,88 @@
+import { useState, Fragment } from 'react';
+import type { NextPage } from 'next';
+import Link from 'next/link';
+import type { player as PlayerT } from '@prisma/client';
+import cl from 'classnames';
+
+import { prisma } from 'services/db';
+import SuggestionsInput from 'ui-kit/SuggestionsInput';
+import styles from 'styles/H2h.module.scss';
+
+const H2hPage: NextPage<{ allPlayers: PlayerT[] }> = ({ allPlayers }) => {
+  const [selectedPlayers, setPlayers] = useState<PlayerT[]>([]);
+
+  const onSuggestionClick = (p: PlayerT) => {
+    setPlayers((v) => [...v, p]);
+  };
+
+  const reset = () => {
+    setPlayers([]);
+  };
+
+  return (
+    <div className={styles.container}>
+      <p className={styles.pageTitle}>Head To Head</p>
+      <div
+        className={cl(
+          styles.selection,
+          selectedPlayers.length === 0 ? styles.initialSelection : ''
+        )}
+      >
+        {selectedPlayers.length > 0 && (
+          <div>
+            {selectedPlayers.map((p, i) => (
+              <Fragment key={p.id}>
+                <div className={styles.selectedPlayer}>
+                  {(p.first_name as string)[0]}. {p.last_name}
+                </div>
+                {i === 0 && <span className={styles.vs}>vs.</span>}
+              </Fragment>
+            ))}
+          </div>
+        )}
+        <div className={cl(selectedPlayers.length === 0 ? styles.initial : '')}>
+          {selectedPlayers.length !== 2 && (
+            <SuggestionsInput
+              placeholder={`Введите имя ${
+                selectedPlayers.length === 1 ? '2-го' : '1-го'
+              } игрока`}
+              players={allPlayers}
+              onSuggestionClick={onSuggestionClick}
+            />
+          )}
+          <div className={styles.buttons}>
+            <Link
+              href={`/h2h/compare?p1Id=${selectedPlayers[0]?.id}&p2Id=${selectedPlayers[1]?.id}`}
+            >
+              <span
+                className={cl(
+                  styles.compare,
+                  !selectedPlayers[0]?.id && !selectedPlayers[1]?.id
+                    ? styles.disabled
+                    : ''
+                )}
+              >
+                Сравнить
+              </span>
+            </Link>
+            <button onClick={reset} className={styles.reset}>
+              Сбросить
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export const getServerSideProps = async () => {
+  const players = await prisma.player.findMany();
+
+  return {
+    props: {
+      allPlayers: players,
+    },
+  };
+};
+
+export default H2hPage;
