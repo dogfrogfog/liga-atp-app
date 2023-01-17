0eab3a0e8f94a1376c3824d0b3dc66d424d81dc1
Maksim Hodasevich <maksim.hodasevich@gmail.com>
2023-01-17 08:55:18 +0100

    separate digest api

--- pages/api/digests/index.ts
+++ pages/api/digests/index.ts
@@ -8,20 +8,8 @@ export default async (
   res: NextApiResponse<DigestT[] | DigestT>
 ) => {
   if (req.method === 'GET') {
-    const digests = await prisma.digest.findMany({
-      orderBy: {
-        id: 'desc',
-      },
-    });
+    const digests = await prisma.digest.findMany();
 
     res.json(digests);
-  }
-
-  if (req.method === 'POST') {
-    const newDigest = await prisma.digest.create({
-      data: req.body.data,
-    });
-
-    res.json(newDigest);
   }
 };
