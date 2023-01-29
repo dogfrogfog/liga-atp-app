5c1293e645da547e0f9af72bc48f7afad02ec6d3
Maksim Hodasevich <maksim.hodasevich@gmail.com>
2023-01-29 15:31:29 +0100

    highlight bottom menu on click

--- layouts/MainAppLayout.tsx
+++ layouts/MainAppLayout.tsx
@@ -1,5 +1,4 @@
 import type { ReactNode } from 'react';
-import Link from 'next/link';
 import cl from 'classnames';
 import { useRouter } from 'next/router';
 
@@ -13,62 +12,62 @@ import {
 } from './MainAppLayoutIcons';
 
 function MainAppLayout({ children }: { children: ReactNode }) {
-  const currentRoute = useRouter().pathname;
+  const router = useRouter();
+  const currentRoute = router.pathname;
+
+  const onLinkClick = (url: string) => {
+    router.push(url);
+  };
 
   return (
     <>
       <div className={styles.pageContainer}>{children}</div>
       <div className={styles.bottomMenu}>
-        <Link href="/players">
-          <div
-            className={cl(
-              styles.menuItem,
-              currentRoute === '/players' ? styles.active : styles.passive
-            )}
-          >
-            <Players />
-          </div>
-        </Link>
-        <Link href="/tournaments">
-          <div
-            className={cl(
-              styles.menuItem,
-              currentRoute === '/tournaments' ? styles.active : styles.passive
-            )}
-          >
-            <Tournaments />
-          </div>
-        </Link>
-        <Link href="/digests">
-          <div
-            className={cl(
-              styles.menuItem,
-              currentRoute === '/digests' ? styles.active : ''
-            )}
-          >
-            <Digest />
-          </div>
-        </Link>
-        <Link href="/ranking">
-          <div
-            className={cl(
-              styles.menuItem,
-              currentRoute === '/ranking' ? styles.active : ''
-            )}
-          >
-            <Rating />
-          </div>
-        </Link>
-        <Link href="/h2h">
-          <div
-            className={cl(
-              styles.menuItem,
-              currentRoute === '/h2h' ? styles.active : ''
-            )}
-          >
-            <HeadToHead />
-          </div>
-        </Link>
+        <button
+          onClick={() => onLinkClick('/players')}
+          className={cl(
+            styles.menuItem,
+            currentRoute === '/players' ? styles.active : styles.passive
+          )}
+        >
+          <Players />
+        </button>
+        <button
+          onClick={() => onLinkClick('/tournaments')}
+          className={cl(
+            styles.menuItem,
+            currentRoute === '/tournaments' ? styles.active : styles.passive
+          )}
+        >
+          <Tournaments />
+        </button>
+        <button
+          onClick={() => onLinkClick('/digests')}
+          className={cl(
+            styles.menuItem,
+            currentRoute === '/digests' ? styles.active : ''
+          )}
+        >
+          <Digest />
+        </button>
+        <button
+          onClick={() => onLinkClick('/ranking')}
+          className={cl(
+            styles.menuItem,
+            currentRoute === '/ranking' ? styles.active : ''
+          )}
+        >
+          <Rating />
+        </button>
+        <button
+          onClick={() => onLinkClick('/h2h')}
+          className={cl(
+            styles.menuItem,
+            currentRoute === '/h2h' ? styles.active : ''
+          )}
+        >
+          <HeadToHead />
+        </button>
       </div>
     </>
   );
