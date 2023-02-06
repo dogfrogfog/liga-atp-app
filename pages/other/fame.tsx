d638540cb098298f0792a419258395424ed415df
Maksim Hodasevich <maksim.hodasevich@gmail.com>
2023-02-06 17:51:35 +0100

    add markdown preview to others page

--- pages/other/fame.tsx
+++ pages/other/fame.tsx
@@ -1,9 +1,16 @@
 import type { NextPage } from 'next';
+import dynamic from 'next/dynamic';
 
 import { prisma } from 'services/db';
 import PageTitle from 'ui-kit/PageTitle';
+import LoadingSpinner from 'ui-kit/LoadingSpinner';
 import { OTHER_PAGES_KEYS } from 'constants/values';
 import styles from 'styles/Other.module.scss';
+
+const MarkdownPreview = dynamic(
+  () => import('@uiw/react-md-editor').then((mod) => mod.default.Markdown),
+  { ssr: false, loading: () => <LoadingSpinner /> }
+);
 
 type FamePageProps = {
   content: string;
@@ -13,7 +20,9 @@ const FamePage: NextPage<FamePageProps> = ({ content }) => {
   return (
     <div className={styles.pageContainer}>
       <PageTitle>Аллея славы</PageTitle>
-      <div className={styles.markdownContainer}>{content}</div>
+      <div className={styles.markdownContainer}>
+        <MarkdownPreview source={content || ''} />
+      </div>
     </div>
   );
 };
--- pages/other/schedule.tsx
+++ pages/other/schedule.tsx
@@ -1,9 +1,16 @@
 import type { NextPage } from 'next';
+import dynamic from 'next/dynamic';
 
+import LoadingSpinner from 'ui-kit/LoadingSpinner';
 import { prisma } from 'services/db';
 import PageTitle from 'ui-kit/PageTitle';
 import { OTHER_PAGES_KEYS } from 'constants/values';
 import styles from 'styles/Other.module.scss';
+
+const MarkdownPreview = dynamic(
+  () => import('@uiw/react-md-editor').then((mod) => mod.default.Markdown),
+  { ssr: false, loading: () => <LoadingSpinner /> }
+);
 
 type SchedulePageProps = {
   content: string;
@@ -13,7 +20,9 @@ const SchedulePage: NextPage<SchedulePageProps> = ({ content }) => {
   return (
     <div className={styles.pageContainer}>
       <PageTitle>Расписание</PageTitle>
-      <div className={styles.markdownContainer}>{content}</div>
+      <div className={styles.markdownContainer}>
+        <MarkdownPreview source={content || ''} />
+      </div>
     </div>
   );
 };
