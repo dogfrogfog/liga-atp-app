f3d0929fea07fe2b04d5e3f3c98790a44ee7d083
Maksim Hodasevich <maksim.hodasevich@gmail.com>
2023-02-11 10:06:05 +0100

    fix sidebar tags

--- layouts/components/Sidebar.tsx
+++ layouts/components/Sidebar.tsx
@@ -4,31 +4,39 @@ import { TbNews } from 'react-icons/tb';
 import { GiBabyfootPlayers } from 'react-icons/gi';
 import { FiMoreHorizontal } from 'react-icons/fi';
 
-import styles from '../../styles/Sidebar.module.scss';
+import styles from 'styles/Sidebar.module.scss';
 
 const Sidebar = () => {
   return (
     <div className={styles.sidebarContainer}>
       <div className={styles.sidebarTop}>
-        <div className={styles.menuItem}>
-          <Link href="/admin">Главная</Link>
-        </div>
-        <div className={styles.menuItem}>
-          <GiBabyfootPlayers />
-          <Link href="/admin/players">Игроки</Link>
-        </div>
-        <div className={styles.menuItem}>
-          <AiOutlineTrophy />
-          <Link href="/admin/tournaments">Турниры</Link>
-        </div>
-        <div className={styles.menuItem}>
-          <TbNews />
-          <Link href="/admin/digests">Дайджест</Link>
-        </div>
-        <div className={styles.menuItem}>
-          <FiMoreHorizontal />
-          <Link href="/admin/other">Прочее</Link>
-        </div>
+        <Link href="/admin">
+          <a className={styles.menuItem}>Главная</a>
+        </Link>
+        <Link href="/admin/players">
+          <a className={styles.menuItem}>
+            <GiBabyfootPlayers />
+            Игроки
+          </a>
+        </Link>
+        <Link href="/admin/tournaments">
+          <a className={styles.menuItem}>
+            <AiOutlineTrophy />
+            Турниры
+          </a>
+        </Link>
+        <Link href="/admin/digests">
+          <a className={styles.menuItem}>
+            <TbNews />
+            Дайджест
+          </a>
+        </Link>
+        <Link href="/admin/other">
+          <a className={styles.menuItem}>
+            <FiMoreHorizontal />
+            Прочее
+          </a>
+        </Link>
       </div>
     </div>
   );
