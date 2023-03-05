ee072ac36ef702f31fe0d1068e65dfea98ccd423
Maksim Hodasevich <maksim.hodasevich@gmail.com>
2023-03-05 20:38:15 +0100

    add spinner overlay

--- components/LoadingShadow.tsx
+++ components/LoadingShadow.tsx
@@ -1,5 +1,12 @@
+import LoadingSpinner from 'ui-kit/LoadingSpinner';
+
 import styles from './LoadingShadow.module.scss';
 
-const LoadingShadow = () => <div className={styles.loadingShadow}></div>;
+const LoadingShadow = () =>
+  <div className={styles.loadingShadow}>
+    <div className={styles.spinner}>
+      <LoadingSpinner />
+    </div>
+  </div>;
 
 export default LoadingShadow;
