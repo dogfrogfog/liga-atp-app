57c907e0f1cf5b568d814f6d4b7bb585c743857c
maksim hodasevich <47758224+dogfrogfog@users.noreply.github.com>
2023-03-06 19:08:03 +0100

    feat: add home page packground image and change desc (#152)

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,12 +4,20 @@ import styles from 'styles/Home.module.scss';
 
 const HomePage: NextPage = () => {
   return (
-    <div className={styles.description}>
-      <h3 className={styles.previeTitle}>Лига тенниса</h3>
-      <p className={styles.secondaryDesc}>
-        Лига Тенниса - это турниры по теннису различной категории для новичков,
-        любителей и профессионалов в Минске
-      </p>
+    <div className={styles.homePage}>
+      <div className={styles.description}>
+        <h3 className={styles.previeTitle}>ЛИГА ТЕННИСА</h3>
+        <p className={styles.secondaryDesc}>
+          Комьюнити классных людей вокруг тенниса с еженедельными турнирами для
+          новичков, любителей и бывших про Более 2000 участников.
+          <br />
+          <br />
+          Онлайн-трансляции, статистика и история всех матчей.
+          <br />
+          <br />
+          Основана в 2013 г
+        </p>
+      </div>
     </div>
   );
 };
