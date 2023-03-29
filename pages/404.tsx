bb680fc183059469c6185ee98c8c231afa0b23da
dogfrogfog <maksim.hodasevich@gmail.com>
2023-03-29 07:16:14 +0200

    add error page styles

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,7 +1,15 @@
 import type { NextPage } from 'next';
 
+import styles from 'styles/errorPage.module.scss';
+
 const FourZeroFour: NextPage = () => {
-  return <h1>404</h1>;
+  return (
+    <div className={styles.pageContainer}>
+      <p className={styles.mainText}>Упс....ошибка 404</p>
+      <p className={styles.secondaryText}>Что-то пошло не так</p>
+      <p className={styles.secondaryText}>попробуйте позже</p>
+    </div>
+  );
 };
 
 export default FourZeroFour;
--- pages/505.tsx
+++ pages/505.tsx
@@ -1,7 +1,14 @@
 import type { NextPage } from 'next';
 
+import styles from 'styles/errorPage.module.scss';
+
 const FiveZeroFive: NextPage = () => {
-  return <h1>505</h1>;
+  return (
+    <div className={styles.pageContainer}>
+      <p className={styles.mainText}>Упс....ошибка сервера</p>
+      <p className={styles.secondaryText}>Что-то пошло не так</p>
+    </div>
+  );
 };
 
 export default FiveZeroFive;
