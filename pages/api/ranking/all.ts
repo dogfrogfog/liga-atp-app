5b50aec5be9465116f146d9009ae0d9987779f3b
maksim hodasevich <47758224+dogfrogfog@users.noreply.github.com>
2023-04-05 20:29:09 +0200

    add all players to main players page (#164)
    * add all players to main players page
    
    * fix types

--- pages/api/ranking/all.ts
+++ pages/api/ranking/all.ts
@@ -4,7 +4,12 @@ import { prisma } from 'services/db';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'GET') {
-    const playersEloRankings = await prisma.player_elo_ranking.findMany();
+    const playersEloRankings = await prisma.player_elo_ranking.findMany({
+      select: {
+        player_id: true,
+        elo_points: true,
+      },
+    });
 
     res.json(playersEloRankings);
   }
