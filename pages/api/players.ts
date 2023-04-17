981dc687d2fae6b84e21809308df67c1348b31e1
dogfrogfog <maksim.hodasevich@gmail.com>
2023-04-17 20:46:22 +0200

    add all players api

---
+++ pages/api/players.ts
@@ -0,0 +1,15 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { Prisma, player as PlayerT } from '@prisma/client';
+
+import { prisma } from 'services/db';
+
+export default async (
+  req: NextApiRequest,
+  res: NextApiResponse<PlayerT[] | PlayerT | Prisma.BatchPayload>
+) => {
+  if (req.method === 'GET') {
+    const players = await prisma.player.findMany();
+
+    res.json(players);
+  }
+}
