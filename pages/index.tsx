ba09d3c33e18c4df58242fa045b190b904bd0534
Pavel <drabovich.official@gmail.com>
2023-04-27 21:03:43 +0300

    added btn install

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,45 +1,22 @@
 import { NextPage } from 'next';
-import { useState, useEffect } from 'react';
-import {GoCloudDownload} from 'react-icons/go'
+// import { useState, useEffect } from 'react';
+// import {GoCloudDownload} from 'react-icons/go'
 
 import styles from 'styles/Home.module.scss';
 
 
 const HomePage: NextPage = () => {
 
-  const [deferredPrompt, setDeferredPrompt] = useState<any>(null);
-
-  useEffect(() => {
-    console.log("work");
-    const handleBeforeInstallPrompt = (event: Event) => {
-      event.preventDefault();
-      setDeferredPrompt(event);
-    };
-
-    window.addEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
-
-    return () => {
-      window.removeEventListener('beforeinstallprompt', handleBeforeInstallPrompt);
-    };
-  }, []);
-
-  const handleInstallClick = async () => {
-    if (deferredPrompt) {
-      deferredPrompt.prompt();
-      const choiceResult = await deferredPrompt.userChoice;
-      console.log(`User ${choiceResult.outcome}`);
-      setDeferredPrompt(null);
-    }
-  };
+  
 
   return (
     <div className={styles.homePage}>
       <div className={styles.description}>
-        {deferredPrompt &&
+        {/* {deferredPrompt &&
           <a onClick={handleInstallClick}>
             <GoCloudDownload size={35} color="#4cc4d1" />
           </a>
-        }
+        } */}
         <h3 className={styles.previeTitle}>ЛИГА ТЕННИСА</h3>
         <p className={styles.secondaryDesc}>
           Комьюнити классных людей вокруг тенниса с еженедельными турнирами для
