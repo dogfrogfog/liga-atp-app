110f294283eb29800598e424330f2123fac4fb9d
Pavel <drabovich.official@gmail.com>
2023-04-28 15:38:44 +0300

    changed app installer

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
 import { NextPage } from 'next';
-import { useState, useEffect } from 'react';
-import {GoCloudDownload} from 'react-icons/go'
+import {useState, useEffect} from 'react';
+import {BsApple} from 'react-icons/bs';
+import {DiAndroid} from 'react-icons/di';
+import {MdIosShare} from 'react-icons/md';
+import {HiDotsVertical} from 'react-icons/hi';
 
 import styles from 'styles/Home.module.scss';
 
@@ -8,9 +11,10 @@ import styles from 'styles/Home.module.scss';
 const HomePage: NextPage = () => {
 
   const [deferredPrompt, setDeferredPrompt] = useState<any>(null);
+  const [isShowInfoIOS, setisShowInfoIOS] = useState<boolean>(false);
+  const [isShowInfoAndroid, setisShowInfoAndroid] = useState<boolean>(false);
 
   useEffect(() => {
-    console.log("work");
     const handleBeforeInstallPrompt = (event: Event) => {
       event.preventDefault();
       setDeferredPrompt(event);
@@ -32,14 +36,72 @@ const HomePage: NextPage = () => {
     }
   };
 
+  const handleShowInfoIOS = () => {
+    if(isShowInfoAndroid) {
+      setisShowInfoAndroid(false);
+    }
+    setisShowInfoIOS(!isShowInfoIOS)
+  }
+
+  const handleShowInfoAndroid = () => {
+    if(isShowInfoIOS) {
+      setisShowInfoIOS(false);
+    }
+    setisShowInfoAndroid(!isShowInfoAndroid);
+  }
+
   return (
     <div className={styles.homePage}>
+      <div className={styles.wrapperIcons}>
+        {deferredPrompt ? (
+          <>
+            <a 
+              className={styles.installIconIOS} 
+              onClick={handleInstallClick}>
+                <BsApple />
+            </a>
+            <a 
+              className={styles.installIconAndroid} 
+              onClick={handleInstallClick}>
+                <DiAndroid />
+            </a>
+          </>
+          ) : (
+            <>
+              <a 
+                className={`${styles.infoIconIOS} ${isShowInfoIOS ? styles.activeIcon : ''}`}>
+                  <BsApple onClick={handleShowInfoIOS} />
+                {
+                  isShowInfoIOS && (
+                    <p className={styles.infoIOS}>
+                      Для установки <br />
+                      приложения <br />
+                      на IOS, нажмите на <MdIosShare className={styles.iconText} /> <br />
+                      внизу экрана, а затем <br />
+                      на экран "Домой"
+                    </p>
+                  )
+                }
+              </a>
+              <a 
+                className={`${styles.infoIconAndroid} ${isShowInfoAndroid ? styles.activeIcon : ''}`}>
+                  <DiAndroid onClick={handleShowInfoAndroid} />
+                {
+                  isShowInfoAndroid && (
+                    <p className={styles.infoAndroid}>
+                      Для установки <br />
+                      приложения <br />
+                      на ANDROID, нажмите вверху экрана 
+                      <HiDotsVertical className={styles.iconText} /> <br />
+                      а затем "Установить приложение"
+                    </p>
+                  )
+                }
+              </a>
+            </>
+        )}
+      </div>
       <div className={styles.description}>
-        {deferredPrompt &&
-          <a onClick={handleInstallClick}>
-            <GoCloudDownload size={35} color="#4cc4d1" />
-          </a>
-        }
         <h3 className={styles.previeTitle}>ЛИГА ТЕННИСА</h3>
         <p className={styles.secondaryDesc}>
           Комьюнити классных людей вокруг тенниса с еженедельными турнирами для
