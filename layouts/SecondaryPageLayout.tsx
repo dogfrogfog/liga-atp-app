7e858dfabcf04fef6e043c37135f91a275cad49f
Maxim Remez <remezmaxim1992@gmail.com>
2023-04-29 13:46:35 +0300

    fix: add swiping to return to main menu(1)

--- layouts/SecondaryPageLayout.tsx
+++ layouts/SecondaryPageLayout.tsx
@@ -5,6 +5,7 @@ import { BiArrowBack } from 'react-icons/bi';
 import LoadingShadow from 'components/LoadingShadow';
 import styles from './SecondaryPageLayout.module.scss';
 import { useSwipeable } from "react-swipeable";
+import { useState } from "react";
 
 function SecondaryLayout({
   children,
@@ -14,12 +15,19 @@ function SecondaryLayout({
   loading: boolean;
 }) {
   const router = useRouter();
-  const handlerSwipe = useSwipeable({
-    onSwipedRight: () => router.push("/")
+  const [swipePosition, setSwipePosition] = useState<number>(0);
+  const swipe = useSwipeable({
+    onSwipedRight: (eventData) => handleSwipe(eventData.deltaX)
   });
+  const handleSwipe = (deltaX: number) => {
+    setSwipePosition((prevPosition) => prevPosition + deltaX);
+    if (swipePosition > window.innerWidth * 0.75){
+      router.push("/");
+    }
+  };
 
   return (
-    <div className={styles.pageContainer} {...handlerSwipe}>
+    <div className={styles.pageContainer} {...swipe}>
       <button className={styles.back} onClick={() => router.back()}>
         {loading && <LoadingShadow />}
         <BiArrowBack size="xl" />
