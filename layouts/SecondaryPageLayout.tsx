307203c29ba87bc145a921e497977471f486d91f
Maxim Remez <remezmaxim1992@gmail.com>
2023-05-03 17:55:29 +0300

    fix: add button home

--- layouts/SecondaryPageLayout.tsx
+++ layouts/SecondaryPageLayout.tsx
@@ -1,6 +1,6 @@
 import type { ReactNode } from 'react';
 import { useRouter } from 'next/router';
-import { BiArrowBack } from 'react-icons/bi';
+import { BiArrowBack, BiHomeCircle } from 'react-icons/bi';
 
 import LoadingShadow from 'components/LoadingShadow';
 import styles from './SecondaryPageLayout.module.scss';
@@ -19,6 +19,10 @@ function SecondaryLayout({
       <button className={styles.back} onClick={() => router.back()}>
         {loading && <LoadingShadow />}
         <BiArrowBack size="xl" />
+      </button>
+      <button className={styles.home} onClick={() => router.push("/")}>
+        {loading && <LoadingShadow />}
+        <BiHomeCircle size="xl" />
       </button>
       {children}
     </div>
