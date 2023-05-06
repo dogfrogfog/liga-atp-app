b08b4c1ce35dc53d14b1c643804f7eb1e2b39759
Maxim Remez <remezmaxim1992@gmail.com>
2023-05-06 14:42:30 +0300

    fix: fixed navigation for ranking (#231)

---
+++ constants/playerLevel.ts
@@ -0,0 +1,11 @@
+export const enum PlayerLevel {
+    Hype = "hype",
+    Pro = "pro",
+    SuperMasters = "super-masters",
+    Masters = "masters",
+    Challenger = "challenger",
+    Legger = "legger",
+    Futures = "futures",
+    Satellite = "satellite"
+
+}
