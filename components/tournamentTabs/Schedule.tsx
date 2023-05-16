81af8f411e93489007c18c2a312ba0bac024b735
jjshko10 <staslukyanenko1995@gmail.com>
2023-05-17 02:20:52 +0300

    var

--- components/tournamentTabs/Schedule.tsx
+++ components/tournamentTabs/Schedule.tsx
@@ -88,20 +88,20 @@ const ScheduleTab = forwardRef<any, ScheduleTabProps>(
                   return;
                 }
                 
-                if (diff < 105 && diff > -105) {
+                if (diff < 102 && diff > -102) {
                   console.log('not moved stage not changed');
                   setTouchStart(null);
                   return;
                 }
 
-                if (diff >= 105) {
+                if (diff >= 102) {
                   setActiveStage(i + 1)
                   setTouchStart(null);
                   console.log('moved right');
                   return;
                 }
 
-                if (diff <= 105) {
+                if (diff <= 102) {
                   setActiveStage(i - 1)
                   setTouchStart(null);
                   console.log('moved left');
